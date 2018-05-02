059cc78d1a1c821ad97de4e6c38298099d23d67d
golok93 <golokmd@gmail.com>
2018-05-02 20:37:42 +0300

    Finish animated menu

--- src/components/common/Menu/Menu.js
+++ src/components/common/Menu/Menu.js
@@ -6,14 +6,24 @@ const ROOT_CLASS = 'menu';
 const ITEMS = ['terminal', 'marketplace', 'billing', 'profile', 'help'];
 
 export const Menu = (props) => {
-  console.log(props);
   const buildRootClass = () => {
     return `${ROOT_CLASS} ${ROOT_CLASS}_${props.page}`;
   }
 
+  const logoClass = () => {
+    // return `${ROOT_CLASS}__logo`;
+    let result = `${ROOT_CLASS}__logo`;
+    if (props.exPage === 'profile' && props.page !== 'profile') {
+      result += ` ${ROOT_CLASS}__logo_close-logo`;
+    } else if (props.page === 'profile' && props.exPage !== 'profile') {
+      result += ` ${ROOT_CLASS}__logo_open-logo`;
+    }
+    return result;
+  }
+
   return (
     <div className={buildRootClass()}>
-      <div className={`${ROOT_CLASS}__logo`}></div>
+      <div className={logoClass()}></div>
       {ITEMS.map((item, index) =>
         <div className={`${ROOT_CLASS}__item-wrap`} key={index}>
           <MenuItem
