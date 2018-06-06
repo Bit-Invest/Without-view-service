6e99a7b17a68ff10d4a381c3bc95e1c9672b6d38
Dev <dev_1@crt.local>
2018-06-06 17:08:33 +0300

    Start pop up manager, start terminal fixes

--- src/store/modules/common/common.js
+++ src/store/modules/common/common.js
@@ -1,13 +1,19 @@
 import { LocalStorage } from '@common/Utils';
 
 export const CHECK_JWT = 'common/CHECK_JWT';
+export const SHOW_POP_UP = 'common/SHOW_POP_UP';
+export const HIDE_POP_UP = 'common/HIDE_POP_UP';
 
 const initialState = {
-
+  currentPopUp: null
 };
 
 export const common = (state = initialState, action) => {
   switch (action.type) {
+    case (HIDE_POP_UP):
+      return Object.assign({}, state, {currentPopUp: null});
+    case (SHOW_POP_UP):
+      return Object.assign({}, state, {currentPopUp: action.payload.popUp});
     default:
       return state;
   }
@@ -26,4 +32,19 @@ export const checkJWT = () => {
       }
     }
   }
+}
+
+export const showPopUp = (popUp) => {
+  return {
+    type: SHOW_POP_UP,
+    payload: {
+      popUp
+    }
+  };
+}
+
+export const hidePopUp = () => {
+  return {
+    type: HIDE_POP_UP
+  };
 }
