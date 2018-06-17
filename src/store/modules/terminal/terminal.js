300e28104a5a18cefc720a1afa55cd2d68a4dfcc
Dev <dev_1@crt.local>
2018-06-17 18:17:12 +0300

    Add place limit order

--- src/store/modules/terminal/terminal.js
+++ src/store/modules/terminal/terminal.js
@@ -1,15 +1,30 @@
-// import { LocalStorage } from '@common/Utils';
+import { LocalStorage } from '@common/Utils';
+
+export const PLACE_LIMIT_ORDER = 'terminal/PLACE_LIMIT_ORDER';
 
 const initialState = {};
-
-export const OPEN_POSITION = 'terminal/OPEN_POSITION';
-export const OPEN_POSITION_SUCCESS = 'terminal/OPEN_POSITION_SUCCESS';
-export const TEST_SUBSCRIBE = 'terminal/TEST_SUBSCRIBE';
-export const LOG_TEST = 'terminal/LOG_TEST';
 
 export const terminal = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
+  }
+}
+
+export const placeLimitOrder = (orderData) => {
+  return {
+    type: PLACE_LIMIT_ORDER,
+    payload: {
+      socket: {
+        type: 'emit',
+        message: 'ORDER_LIMIT',
+        payload: {
+          jwt: LocalStorage.getItem('token'),
+          endpoint: {
+            payload: orderData
+          }
+        }
+      }
+    }
   }
 }
