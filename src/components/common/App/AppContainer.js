ee9537a0baac2754a2fa30255f653ddca178c66c
Khaniev <haniev91@mail.ru>
2018-09-04 12:45:13 +0300

    appended Tour to MarketPlace and replace back-end logic for MarketPlace

--- src/components/common/App/AppContainer.js
+++ src/components/common/App/AppContainer.js
@@ -5,100 +5,18 @@ import { connect } from 'react-redux';
 import { checkJWT } from '@store/modules/common';
 import { bindActionCreators } from 'redux';
 import { push } from 'react-router-redux';
-import Tour from 'reactour';
 
 class AppContainer extends React.Component {
   constructor() {
-    super()
-    this.state = {
-      isTourOpen: false,
-      isShowingMore: false,
-    }
-  }
-
-  componentDidMount() {
-    setTimeout(()=>{
-      this.setState({
-        isTourOpen: true
-      });
-    }, 2000);
-  }
-
-  toggleShowMore = () => {
-    this.setState(prevState => ({
-      isShowingMore: !prevState.isShowingMore,
-    }))
-  }
-
-  closeTour = () => {
-    this.setState({ isTourOpen: false })
-  }
-
-  openTour = () => {
-    this.setState({ isTourOpen: true })
+    super();
   }
 
   render() {
-    const { isTourOpen, isShowingMore } = this.state
-    const accentColor = '#5cb7b7'
-
     return (
-      <div>
-        <Tour
-          onRequestClose={this.closeTour}
-          steps={tourConfig}
-          isOpen={isTourOpen}
-          maskClassName="mask"
-          className="helper"
-          rounded={5}
-          accentColor={accentColor}
-        />
-
-        <App />
-      </div>
+      <App />
     )
   }
 }
-
-const tourConfig = [
-  {
-    selector: '.trader-card:nth-of-type(1) ',
-    content: `Product Card`,
-    accentColor: '#f2f2f2'
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-card__chart-wrap.trader-card__chart-wrap_loaded',
-    content: "Indicator of profitability",
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-card__exchange',
-    content: 'Exchange where the product is allocated',
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .user__name-wrap',
-    content: `Investment products provided by asset managers`,
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-card__pair',
-    content: `Basic asset`,
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-card__units',
-    content: `Profitability of the strategy`,
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(1)',
-    content: `The period of implementation`,
-  },
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(2)',
-    content: `The fees to be paid to asset manager for successful trades`,
-  }, 
-  {
-    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(3)',
-    content: `The number of investors connected`,
-  },
-];
 
 const mapStateToProps = (state) => {
   return {pathname: state.router.location.pathname};
--- src/components/marketplace/MarketplacePage/MarketplacePageContainer.js
+++ src/components/marketplace/MarketplacePage/MarketplacePageContainer.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { getProducts } from '@store/modules/marketplace';
 import { LocalStorage } from '@common/Utils';
 import { checkJWT } from '@store/modules/common';
+import Tour from 'reactour';
 import {
   userLogIn,
   getKeys,
@@ -17,18 +18,69 @@ const ShowTypes = {
   LIST_ELEM: 'LIST_ELEM'
 };
 
+const tourConfig = [
+  {
+    selector: '.trader-card:nth-of-type(1) ',
+    content: `Product Card`,
+    accentColor: '#f2f2f2'
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-card__chart-wrap.trader-card__chart-wrap_loaded',
+    content: "Indicator of profitability",
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-card__exchange',
+    content: 'Exchange where the product is allocated',
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .user__name-wrap',
+    content: `Investment products provided by asset managers`,
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-card__pair',
+    content: `Basic asset`,
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-card__units',
+    content: `Profitability of the strategy`,
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(1)',
+    content: `The period of implementation`,
+  },
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(2)',
+    content: `The fees to be paid to asset manager for successful trades`,
+  }, 
+  {
+    selector: '.trader-card:nth-of-type(1) .trader-stat:nth-of-type(3)',
+    content: `The number of investors connected`,
+  },
+];
+
 class MarketplacePageContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoaded: false,
-      showType: ShowTypes.CARD
+      showType: ShowTypes.CARD,
+
+      isTourOpen: false,
+      isShowingMore: false,
     };
   }
 
   componentWillMount() {
-    this.props.getProducts()
-      .then(this.afterLoading.bind(this));
+    const { products } = this.props;
+
+    if (!products.length) {
+      this.props.getProducts()
+        .then(this.afterLoading.bind(this));
+    } else {
+      setTimeout(()=>{ // для симуляции подтягивания данных
+        this.callbackLoaded();
+      },500);
+    }
   }
 
   afterLoading() {
@@ -39,22 +91,57 @@ class MarketplacePageContainer extends React.Component {
         .then(this.props.getKeys)
         .then(this.props.getSubscribedProducts)
         .then(this.props.getPersonalInfo)
-        .then(() => {this.setState({isLoaded: true});})
+        .then(() => {this.callbackLoaded()})
     } else {
-      this.setState({isLoaded: true});
+      this.callbackLoaded();
     }
   }
 
+  callbackLoaded = () => {
+    this.setState({
+      isLoaded: true,
+      isTourOpen: true
+    });
+  }
+
+  toggleShowMore = () => {
+    this.setState(prevState => ({
+      isShowingMore: !prevState.isShowingMore,
+    }))
+  }
+
+  closeTour = () => {
+    this.setState({ isTourOpen: false })
+  }
+
+  openTour = () => {
+    this.setState({ isTourOpen: true })
+  }
+
   render() {
+    const { isTourOpen, isShowingMore } = this.state;
+    const accentColor = '#5cb7b7';
+
     return (
-      <MarketplacePage
-        onClickCard={this.onClickCard.bind(this)}
-        onClickList={this.onClickList.bind(this)}
-        isLoaded={this.state.isLoaded}
-        isShowedPopUp={this.state.isShowedPopUp}
-        showType={this.state.showType}
-        cards={this.props.products}
-      />
+      <React.Fragment>
+        <Tour
+          onRequestClose={this.closeTour}
+          steps={tourConfig}
+          isOpen={isTourOpen}
+          maskClassName="mask"
+          className="helper"
+          rounded={5}
+          accentColor={accentColor}
+        />
+        <MarketplacePage
+          onClickCard={this.onClickCard.bind(this)}
+          onClickList={this.onClickList.bind(this)}
+          isLoaded={this.state.isLoaded}
+          isShowedPopUp={this.state.isShowedPopUp}
+          showType={this.state.showType}
+          cards={this.props.products}
+        />
+      </React.Fragment>
     );
   }
 
