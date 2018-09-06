1d6c709818594bfa1b4200396e116d09dd36a02f
Alexey <lolokovalex@mail.ru>
2018-09-06 20:43:43 +0300

    Add fixes: scroll, select.

--- src/components/common/PopUpManager/PopUpManager.js
+++ src/components/common/PopUpManager/PopUpManager.js
@@ -8,13 +8,6 @@ export const PopUpManager = (props) => {
   }
   return (
     <div className={buildRootClass()}>
-      <div onClick={props.onClickClose} className={`${ROOT_CLASS}__undercover`}>
-        <div className={`${ROOT_CLASS}__content-wrap ${ROOT_CLASS}__for-background-click`}>
-          <div className={`${ROOT_CLASS}__content`}>
-            {props.children}
-          </div>
-        </div>
-      </div>
       <div className={`${ROOT_CLASS}__content-wrap`}>
         <div onClick={props.onClickClose} className={`${ROOT_CLASS}__close`}>
           <div className={`${ROOT_CLASS}__check`} alt="close"></div>
