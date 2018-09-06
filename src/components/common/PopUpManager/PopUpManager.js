5b31a8fab021458b06024798bdb4bb45104fbe62
Alexey <lolokovalex@mail.ru>
2018-09-06 12:30:04 +0300

    Add fixed scroll in popUp.

--- src/components/common/PopUpManager/PopUpManager.js
+++ src/components/common/PopUpManager/PopUpManager.js
@@ -9,6 +9,11 @@ export const PopUpManager = (props) => {
   return (
     <div className={buildRootClass()}>
       <div onClick={props.onClickClose} className={`${ROOT_CLASS}__undercover`}>
+        <div className={`${ROOT_CLASS}__content-wrap ${ROOT_CLASS}__for-background-click`}>
+          <div className={`${ROOT_CLASS}__content`}>
+            {props.children}
+          </div>
+        </div>
       </div>
       <div className={`${ROOT_CLASS}__content-wrap`}>
         <div onClick={props.onClickClose} className={`${ROOT_CLASS}__close`}>
