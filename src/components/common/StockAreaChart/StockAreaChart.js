996e090e59022c2826b53b964fccf6f6e4b1fef5
Alexey <lolokovalex@mail.ru>
2018-09-07 18:31:17 +0300

    Add icon.

--- src/components/common/StockAreaChart/StockAreaChart.js
+++ src/components/common/StockAreaChart/StockAreaChart.js
@@ -76,7 +76,7 @@ export class StockAreaChart extends React.Component {
     const xAccessor = d => d.date
     return this.props.data && this.props.data.length > 2 ? (
       <div className={ROOT_CLASS}>
-        { this.props.isTermanal ? <button className="stock-area-chart__drawing-line" onClick={() => this.setState({enableTrendLine: true})}>Drawing Line</button> : null }
+        { this.props.isTermanal ? <button className="stock-area-chart__drawing-line" onClick={() => this.setState({enableTrendLine: true})}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" width="28" height="28"><g fillRule="nonzero"><path d="M7.354 21.354l14-14-.707-.707-14 14z"></path><path d="M22.5 7c.828 0 1.5-.672 1.5-1.5s-.672-1.5-1.5-1.5-1.5.672-1.5 1.5.672 1.5 1.5 1.5zm0 1c-1.381 0-2.5-1.119-2.5-2.5s1.119-2.5 2.5-2.5 2.5 1.119 2.5 2.5-1.119 2.5-2.5 2.5zM5.5 24c.828 0 1.5-.672 1.5-1.5s-.672-1.5-1.5-1.5-1.5.672-1.5 1.5.672 1.5 1.5 1.5zm0 1c-1.381 0-2.5-1.119-2.5-2.5s1.119-2.5 2.5-2.5 2.5 1.119 2.5 2.5-1.119 2.5-2.5 2.5z"></path></g></svg></button> : null }
         <ChartCanvas
           width={width}
           height={height}
