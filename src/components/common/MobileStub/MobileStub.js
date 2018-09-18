3d12d65759a25cfd3ca48e6b9ea1555b5d62bbfc
Alexey <lolokovalex@mail.ru>
2018-09-18 15:38:22 +0300

    Add new mobile stub.

--- src/components/common/MobileStub/MobileStub.js
+++ src/components/common/MobileStub/MobileStub.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import logo from '../../../assets/images/Logo_White_with_title.svg'
 import video from '../../../assets/images/new_mainBlock_screen.png'
+import video2 from '../../../assets/images/mainBlock_screen.png'
+import play_button_youtube from '../../../assets/images/play_button_youtube.png'
 import bitcointalk_icon from '../../../assets/images/socials/bitcointalk_icon.png';
 import youtube_icon from '../../../assets/images/socials/youtube_icon.png'
 import instagram_icon from '../../../assets/images/socials/instagram_icon.png'
@@ -12,29 +14,94 @@ import medium_icon from '../../../assets/images/socials/medium_icon.png'
 import linkedin_icon from '../../../assets/images/socials/linkedin_icon.png'
 import github_icon from '../../../assets/images/socials/github_icon.png'
 import weibo_icon from '../../../assets/images/socials/weibo_icon.png'
+import Slider from 'react-slick'
 
 import { objectLangs, lng } from '../../../lngs/index'
 
 const ROOT_NAME = 'mobile-stub'
 
+const dataArr = [
+    {
+        header: objectLangs[lng]['MobileStub#5'],
+        watch: objectLangs[lng]['MobileStub#2'],
+        video: video,
+        videoSrc: "https://player.vimeo.com/video/286746544",
+        playButton: false,
+        isPlayVideo: false
+    },
+    {
+        header: objectLangs[lng]['MobileStub#1'],
+        watch: objectLangs[lng]['MobileStub#2'],
+        video: video2,
+        videoSrc: "https://www.youtube.com/embed/nmh_BfGHORE",
+        playButton: true,
+        isPlayVideo: false
+    },
+]
+
 class MobileStub extends Component {
     state = {
-        isOpenFrame: false
+        dataArr: dataArr
+    }
+
+    playVideo = (i) => {
+        const newArr = [...this.state.dataArr].map((el, elI) => (elI === i) ? { ...el, isPlayVideo: true } : { ...el })
+        this.setState({
+            dataArr: newArr
+        })
+    }
+
+    renderStub = () => {
+        return this.state.dataArr.map((el, i) => {
+            return (
+                <div key={i} >
+                    <div className={`${ROOT_NAME}__key-wrapper`}>
+                        <div className={`${ROOT_NAME}__header`}>
+                            { el.header }
+                        </div>
+                        <div className={`${ROOT_NAME}__block-video`}>
+                            <div className={`${ROOT_NAME}__watch`}>{ el.watch }</div>
+                            <div className={`${ROOT_NAME}__video`}>
+                                { el.isPlayVideo ?
+                                    <iframe src={`${el.videoSrc}`} frameBorder="0" allowFullScreen></iframe> :
+                                    <div className={`${ROOT_NAME}__video`} onClick={() => this.playVideo(i)} style={{ backgroundImage: `url(${el.video})` }}>
+                                        { el.playButton ?
+                                            <img
+                                                src={play_button_youtube}
+                                                alt="CINDEX"
+                                                className={`${ROOT_NAME}__video-play`}
+
+                                            /> :
+                                            null
+                                        }
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
     }
 
     render() {
+        const settings = {
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            swipeToSlide: true,
+            dots: true,
+            arrows: false
+        }
+
         return (
             <div className={ROOT_NAME}>
                 <img src={logo} className={`${ROOT_NAME}__logo`} />
-                <div className={`${ROOT_NAME}__header`}>{ objectLangs[lng]['MobileStub#5'] }</div>
-                <div className={`${ROOT_NAME}__block-video`}>
-                    <div className={`${ROOT_NAME}__watch`}>{ objectLangs[lng]['MobileStub#2'] }</div>
-                    <div className={`${ROOT_NAME}__video`}>
-                        { this.state.isOpenFrame ?
-                            <iframe src="https://player.vimeo.com/video/286746544?autoplay=1" frameBorder="0" allowFullScreen></iframe> :
-                            <img className={`${ROOT_NAME}__video`} src={video} onClick={() => this.setState({ isOpenFrame: true })} />
-                        }
-                    </div>
+                <div className={`${ROOT_NAME}__wrapper-slider`}>
+                    <Slider {...settings}>
+                        { this.renderStub() }
+                    </Slider>
                 </div>
                 <a href="https://my.cindx.io/" target="_blank" className={`${ROOT_NAME}__block-button`}>
                     <div>{ objectLangs[lng]['MobileStub#3'] }</div>
