79b76caaaeca34e3cb61c84d6be1fe72f86a8048
hoano907 <haniev91@mail.ru>
2019-06-23 03:26:51 +0300

    Push IEO version

---
+++ precache-manifest.325e96ba4da79e14870d733b7b3ae949.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "77627f2035457a73e15210d32fa11c02",
+    "url": "/static/media/registration.77627f20.jpeg"
+  },
+  {
+    "revision": "39a2af266cb6e3c87524741a2d67e4fd",
+    "url": "/static/media/iconBinance.39a2af26.png"
+  },
+  {
+    "revision": "32dd20d884ee71bffb9807b07f20026d",
+    "url": "/static/media/Profile_icon.32dd20d8.svg"
+  },
+  {
+    "revision": "333fc97a7cd68572c5df7e3122d36273",
+    "url": "/static/media/Logo_White_with_title.333fc97a.svg"
+  },
+  {
+    "revision": "0397b8fd1fdd1fa82ab413bd372ddbb1",
+    "url": "/static/media/Billing_icon.0397b8fd.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "aed34679a3b3a3958449",
+    "url": "/static/js/main.ec9ed4f2.chunk.js"
+  },
+  {
+    "revision": "f8dbb274c0a58e53d157",
+    "url": "/static/js/2.2b586909.chunk.js"
+  },
+  {
+    "revision": "aed34679a3b3a3958449",
+    "url": "/static/css/main.94eaaeb4.chunk.css"
+  },
+  {
+    "revision": "7186e90491b96e640be3838bf2b64b90",
+    "url": "/index.html"
+  }
+];
