f86ef337d1a91f2a8f08d48f95ea3d2e009cc3d6
hoano907 <haniev91@mail.ru>
2019-07-22 02:30:03 +0300

    Push new version
    * Big update.

---
+++ precache-manifest.24cbfb814db64fac98b474546d16eed4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce1fa48a10e3253d5c41a2ad1cc41948",
+    "url": "/static/media/Geometria-Heavy.ce1fa48a.woff2"
+  },
+  {
+    "revision": "d4d9a930eaf9f275de72",
+    "url": "/static/css/main.cd6ccf0e.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cb2f3b6a4782bca11029",
+    "url": "/static/js/2.31ec5d8f.chunk.js"
+  },
+  {
+    "revision": "39a2af266cb6e3c87524741a2d67e4fd",
+    "url": "/static/media/iconBinance.39a2af26.png"
+  },
+  {
+    "revision": "32dd20d884ee71bffb9807b07f20026d",
+    "url": "/static/media/Profile_icon.32dd20d8.svg"
+  },
+  {
+    "revision": "0397b8fd1fdd1fa82ab413bd372ddbb1",
+    "url": "/static/media/Billing_icon.0397b8fd.svg"
+  },
+  {
+    "revision": "44e820fc80d3977f02f5276a50485085",
+    "url": "/static/media/logout.44e820fc.png"
+  },
+  {
+    "revision": "76a324c42b8c550ed93b7a27b7459dac",
+    "url": "/static/media/settingsIcon.76a324c4.png"
+  },
+  {
+    "revision": "333fc97a7cd68572c5df7e3122d36273",
+    "url": "/static/media/Logo_White_with_title.333fc97a.svg"
+  },
+  {
+    "revision": "77627f2035457a73e15210d32fa11c02",
+    "url": "/static/media/registration.77627f20.jpeg"
+  },
+  {
+    "revision": "5c1d517bcf9e622ba0749a5abfd8c8c1",
+    "url": "/static/media/Geometria-Bold.5c1d517b.woff2"
+  },
+  {
+    "revision": "d4d9a930eaf9f275de72",
+    "url": "/static/js/main.c5c77980.chunk.js"
+  },
+  {
+    "revision": "e0f9d6492c7a3c6dc987a849fcb9cb10",
+    "url": "/static/media/Geometria-Light.e0f9d649.woff2"
+  },
+  {
+    "revision": "e370e8e3219741663b5a172a00cdb2d2",
+    "url": "/static/media/Geometria-Medium.e370e8e3.woff2"
+  },
+  {
+    "revision": "9e4b52e7e1260af0506f9d915b4e6ffe",
+    "url": "/static/media/Geometria-ExtraLightItalic.9e4b52e7.woff2"
+  },
+  {
+    "revision": "2fe5f7508322d964c030e10156e43a14",
+    "url": "/static/media/Geometria.2fe5f750.woff2"
+  },
+  {
+    "revision": "2718c69a1474b2e2b04afb39c8e62e3f",
+    "url": "/static/media/Geometria-Bold.2718c69a.woff"
+  },
+  {
+    "revision": "21ffa8553f379519ac5d80c38e039714",
+    "url": "/static/media/Geometria-Medium.21ffa855.woff"
+  },
+  {
+    "revision": "1707b84cc13511b162afccae1da3c002",
+    "url": "/static/media/Geometria-Light.1707b84c.woff"
+  },
+  {
+    "revision": "86c78b24aa03145dff4576def89fafab",
+    "url": "/static/media/Geometria-Heavy.86c78b24.woff"
+  },
+  {
+    "revision": "f1d8fcc96293c7610aef044bd6a85556",
+    "url": "/static/media/Geometria-ExtraLightItalic.f1d8fcc9.woff"
+  },
+  {
+    "revision": "413f36b0dc9a4e6ef7cc07b5e4014a66",
+    "url": "/static/media/Geometria.413f36b0.woff"
+  },
+  {
+    "revision": "348e16e9e3c7f00740f63a0d7d1ec572",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a900b574e186ffaeea931c340a066f36.js"
+  "/precache-manifest.24cbfb814db64fac98b474546d16eed4.js"
 );
 
 workbox.clientsClaim();
